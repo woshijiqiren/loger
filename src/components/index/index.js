75d8782387542c859c5dc3c76d152d0ac74df6b9
PXL <pengxiaolong@csii.com.cn>
2020-12-31 16:16:11 +0800

    update

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import { Input } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+const { Search } = Input;
+const onSearch = value =>{console.log(value);} 
 export default class index extends Component {
     render() {
         return (
-            <div>
-                <h1>12354312</h1>
+            <div className="main">
+               <h1>12314</h1>
+               <Search placeholder="输入关键词" onSearch={onSearch} enterButton />
             </div>
         );
     };
